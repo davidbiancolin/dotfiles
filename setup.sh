28ad1743e2ff5e1910da75c8b4a154537ddcff54
David Biancolin <biancolin@bwrcrdsl-1.eecs.berkeley.edu>
2015-05-27 16:40:17 -0700

    Add a gitconfig file

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim ssh/config zshrc oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
+files="bash_profile vimrc vim ssh/config zshrc oh-my-zsh tmux.conf"    # list of files/folders to symlink in homedir
 patch_dirs="ssh" #list of folders with metadata that needs to be maintained 
 ##########
 
