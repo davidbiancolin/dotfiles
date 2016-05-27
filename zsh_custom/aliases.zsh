f7963a36fc2300de9d33a1b2f94d1dd25c4fea0f
David Biancolin <david.biancolin@gmail.com>
2016-05-27 15:19:36 -0700

    Add a function to fix git urls from palmer

--- zsh_custom/aliases.zsh
+++ zsh_custom/aliases.zsh
@@ -10,3 +10,8 @@ alias uu='cd ../..'
 alias uuu='cd ../../..'
 alias uuuu='cd ../../../..'
 
+function fix_git_urls() {
+    git_config="$(GIT_EDITOR=echo git config -e)"
+    echo "Fixing https submodule pointers in $git_config"
+    sed 's!https://github.com/!git@github.com:!g' -i $git_config
+}
