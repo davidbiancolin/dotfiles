90311685086d00a5b210524e5eed3b0375c5b4cc
David Biancolin <david.biancolin@gmail.com>
2016-09-09 12:11:15 -0700

    Add functions to reset DISPLAY variables in tmux

--- zsh_custom/environment.zsh
+++ zsh_custom/environment.zsh
@@ -4,3 +4,15 @@ function git_prompt_info() {
   ref=$(git symbolic-ref HEAD 2> /dev/null) || return
     echo "$ZSH_THEME_GIT_PROMPT_PREFIX${ref#refs/heads/}$ZSH_THEME_GIT_PROMPT_SUFFIX"
 }
+
+export DISPLAY_CACHE_DIR=~/.displays/
+function cache_display_variable() {
+  mkdir -p $DISPLAY_CACHE_DIR
+  echo $DISPLAY > $DISPLAY_CACHE_DIR/`uname -n`.display
+}
+
+alias tmux='cache_display_variable && tmux'
+
+function load_display_variable() {
+  export DISPLAY=$(cat $DISPLAY_CACHE_DIR/`uname -n`.display)
+}
