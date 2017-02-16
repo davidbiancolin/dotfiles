689198f67d274064541a1e894659078d862dfd0e
David Biancolin <david.biancolin@gmail.com>
2017-02-16 12:02:41 -0800

    .zshrc should not be aware of dotfiles

--- common.sh
+++ common.sh
@@ -6,7 +6,7 @@
 dir=`pwd`                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 # list of files/folders to symlink in homedir
-files="gitconfig bash_profile vimrc vim ssh/rc ssh/config zshrc oh-my-zsh tmux.conf"
+files="gitconfig bash_profile vimrc vim ssh/rc ssh/config zshrc oh-my-zsh tmux.conf zsh_custom"
 # list of folders with metadata that needs to be maintained
 patch_dirs="ssh"
 ##########
