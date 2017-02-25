bc9f503198ce03c0c53329f48b40cac76690068c
David Biancolin <david.biancolin@gmail.com>
2017-02-25 14:00:25 -0800

    Copy keys

--- cluster_master.sh
+++ cluster_master.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 set -ex
 
-hosts=(a6 a7 a8)
+hosts=(a1 a3 a4)
+
+COPY_PRIVATE_KEY=false
+KEY_NAME=id_rsa
+COPY_ZSHRC_LOCAL=false
 #This does not work for ivy since it lacks many of the packages that the other
 # mill machines have. So long as you build lua once it's fine.
 #hosts=(ivy)
+
+if $COPY_PRIVATE_KEY ; then
+    for host in ${hosts[@]}; do
+        echo $host
+        scp ~/.ssh/$KEY_NAME $host:~/.ssh/
+    done
+fi
+
+if $COPY_ZSHRC_LOCAL ; then
+    for host in ${hosts[@]}; do
+        scp ~/.zshrc_local $host:~/
+    done
+fi
 
 for host in ${hosts[@]}; do
     echo $host
