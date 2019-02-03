2b64335f2d697bca42ee7e9cfb14ad0f472ff5c2
David Biancolin <david.biancolin@gmail.com>
2019-02-03 23:19:13 +0000

    Add some stuff that is common to upgrade amis

---
+++ migrate-manager.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [[ $# -ne 1 ]]; then
+    echo "$0 takes 1 argument: The old manager's IP address"
+    exit 2
+fi
+
+old_manager=$1
+pkey=~/firesim.pem
+
+if [[ ! -f ~/firesim.pem ]]; then
+    echo "Copy over your FireSim private key."
+    exit 2
+fi
+
+scp -i $pkey $old_manager:~/.ssh/config_local ~/.ssh/config_local
+rsync -av -e "ssh -i $pkey" -a $old_manager:~/firesim-* ~
